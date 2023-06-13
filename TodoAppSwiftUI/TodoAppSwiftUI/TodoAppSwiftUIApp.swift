379cd6c444d5048c32a28e9b69dee8ef2898c26b
NicholasPilotto <nicholaspilotto@gmail.com>
2023-06-13 22:21:43 +0200

    SwiftUI project

---
+++ TodoAppSwiftUI/TodoAppSwiftUI/TodoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppSwiftUIApp.swift
+//  TodoAppSwiftUI
+//
+//  Created by Nicholas Pilotto on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
