0aff9af6eea764244a8c771c47d58a0c1697afd5
NicholasPilotto <nicholaspilotto@gmail.com>
2023-06-14 22:21:53 +0200

    Login view design

--- TodoAppSwiftUI/TodoAppSwiftUI/Views/LoginView.swift
+++ TodoAppSwiftUI/TodoAppSwiftUI/Views/LoginView.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct LoginView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @State var email = ""
+  @State var password = ""
+  
+  var body: some View {
+    NavigationView {
+      
+      VStack {
+        HeaderView()
+        Form {
+          TextField("Email Address", text: $email)
+            .textFieldStyle(DefaultTextFieldStyle())
+          SecureField("Password", text: $password)
+            .textFieldStyle(DefaultTextFieldStyle())
+          
+          Button {
+            
+          } label: {
+            ZStack {
+              RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(Color.blue)
+              
+              Text("Log In")
+                .foregroundColor(Color.white)
+                .bold()
+            }
+          }
+          .padding()
+        }
+        VStack {
+          Text("New around here?")
+          NavigationLink("Create an account", destination: RegisterView())
+        }
+        .padding(.bottom, 50)
+        
+        Spacer()
+      }
     }
+  }
 }
 
 struct LoginView_Previews: PreviewProvider {
-    static var previews: some View {
-        LoginView()
-    }
+  static var previews: some View {
+    LoginView()
+  }
 }
--- TodoAppSwiftUI/TodoAppSwiftUI/Views/MainView.swift
+++ TodoAppSwiftUI/TodoAppSwiftUI/Views/MainView.swift
@@ -8,19 +8,15 @@
 import SwiftUI
 
 struct MainView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    NavigationView {
+      LoginView()
     }
+  }
 }
 
 struct MainView_Previews: PreviewProvider {
-    static var previews: some View {
-        MainView()
-    }
+  static var previews: some View {
+    MainView()
+  }
 }
