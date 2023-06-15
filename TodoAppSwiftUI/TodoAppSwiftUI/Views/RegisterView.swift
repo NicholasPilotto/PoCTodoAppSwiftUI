7919647316c8cf14745368018ca13b50379c4d51
NicholasPilotto <nicholaspilotto@gmail.com>
2023-06-15 20:54:59 +0200

    Finish register UI

--- TodoAppSwiftUI/TodoAppSwiftUI/Views/RegisterView.swift
+++ TodoAppSwiftUI/TodoAppSwiftUI/Views/RegisterView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct RegisterView: View {
+  @State var name = ""
+  @State var email = ""
+  @State var password = ""
+  
   var body: some View {
     VStack {
       HeaderView(title: "Register",
                  subtitle: "Start organizing todos",
                  angle: -15,
                  background: .orange)
+      
+      Form {
+        TextField("Full name", text: $name)
+          .textFieldStyle(DefaultTextFieldStyle())
+          .autocorrectionDisabled()
+        
+        TextField("Email address", text: $email)
+          .textFieldStyle(DefaultTextFieldStyle())
+          .autocapitalization(.none)
+          .autocorrectionDisabled()
+        
+        SecureField("Password", text: $password)
+          .textFieldStyle(DefaultTextFieldStyle())
+        
+        TLButton(title: "Create an Account", background: .green, action: {
+          
+        })
+      }
+      .offset(y: -50)
       
       Spacer()
     }
