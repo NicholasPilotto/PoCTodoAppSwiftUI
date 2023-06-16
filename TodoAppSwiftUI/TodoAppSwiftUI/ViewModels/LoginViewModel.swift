324d5ce61f7d06dadea0fdecced290a5b8eb741c
NicholasPilotto <nicholaspilotto@gmail.com>
2023-06-16 20:57:28 +0200

    Validating login data

--- TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/LoginViewModel.swift
+++ TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/LoginViewModel.swift
@@ -10,10 +10,31 @@ import Foundation
 class LoginViewModel: ObservableObject {
   @Published var email = ""
   @Published var password = ""
+  @Published var errorMessage = ""
   
   init() {}
   
-  func logIn() {}
+  func logIn() {
+    guard validate() else {
+      return
+    }
+  }
   
-  func validate() {}
+  private func validate() -> Bool {
+    errorMessage = ""
+  
+    guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
+          isValidEmailAddress(),
+          !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+      errorMessage = "Please fill in all fieldss"
+      return false
+    }
+    
+    return true
+  }
+  
+  private func isValidEmailAddress() -> Bool {
+    let emailPredicate = NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}")
+    return emailPredicate.evaluate(with: email)
+  }
 }
