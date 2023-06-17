2d26ec1912ef0e47ca7df1d34335d5398fab9793
NicholasPilotto <nicholaspilotto@gmail.com>
2023-06-17 14:48:58 +0200

    User model class

--- TodoAppSwiftUI/TodoAppSwiftUI/Models/User.swift
+++ TodoAppSwiftUI/TodoAppSwiftUI/Models/User.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import RealmSwift
+
+class User: Object {
+  @Persisted(primaryKey: true) var _id: String
+  @Persisted var name: String
+  @Persisted var email: String
+  @Persisted var password: String
+  @Persisted var joined: TimeInterval
+  @Persisted var todos: List<ToDoListItem>
+  
+  override init() {}
+  
+  init(name: String, email: String, password: String) {
+    super.init()
+    
+    self._id = UUID().uuidString
+    self.name = name
+    self.email = email
+    self.password = password
+    self.joined = Date().timeIntervalSince1970
+    self.todos = List<ToDoListItem>()
+  }
+}
