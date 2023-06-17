3295b54f5596e2518258860cec3390823de8c632
NicholasPilotto <nicholaspilotto@gmail.com>
2023-06-17 14:50:16 +0200

    Validating register input data

--- TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/RegisterViewModel.swift
+++ TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/RegisterViewModel.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+class RegisterViewModel: ObservableObject {
+  @Published var name = ""
+  @Published var email = ""
+  @Published var password = ""
+  
+  init() {}
+  
+  private func validate() -> Bool  {
+    guard !name.isCompletelyEmpty, email.isValidEmailAddress, !password.isCompletelyEmpty else {
+      return false
+    }
+    
+    guard password.count >= 6 else {
+      return false
+    }
+    
+    insertUserRecord()
+    
+    return true
+  }
+  
+  private func insertUserRecord() {
+    let newUser = User(name: name, email: email, password: password)
+  }
+  
+  func register() {
+    guard validate() else {
+      return
+    }
+  }
+}
