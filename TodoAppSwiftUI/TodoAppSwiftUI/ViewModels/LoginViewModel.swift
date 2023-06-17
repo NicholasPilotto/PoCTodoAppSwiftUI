3030f72489aae54d658c3589f627626935ac0f2d
NicholasPilotto <nicholaspilotto@gmail.com>
2023-06-17 14:49:45 +0200

    Using new string utility to ckeck if a string is empty or an email address is valid

--- TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/LoginViewModel.swift
+++ TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/LoginViewModel.swift
@@ -23,18 +23,11 @@ class LoginViewModel: ObservableObject {
   private func validate() -> Bool {
     errorMessage = ""
   
-    guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
-          isValidEmailAddress(),
-          !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+    guard email.isValidEmailAddress, !password.isCompletelyEmpty else {
       errorMessage = "Please fill in all fieldss"
       return false
     }
     
     return true
-  }
-  
-  private func isValidEmailAddress() -> Bool {
-    let emailPredicate = NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}")
-    return emailPredicate.evaluate(with: email)
   }
 }
