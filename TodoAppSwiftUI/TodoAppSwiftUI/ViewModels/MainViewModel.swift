06d973651a67ffd94c957008bc527349cdb8dd93
NicholasPilotto <nicholaspilotto@gmail.com>
2023-06-19 19:54:44 +0200

    Test db reader

--- TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/MainViewModel.swift
+++ TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/MainViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import RealmSwift
+
+class MainViewModel: ObservableObject {
+  @Published var appDataModel = AppDataModel()
+  
+  public var isSignedIn: Bool {
+    let user = RealmService.shared.fetch(object: User.self)
+      .where {
+        $0._id.equals(appDataModel.userID)
+      }
+    
+    if !user.isEmpty {
+      return true
+    }
+    
+    return false
+  }
+  
+  public func print() {
+    Swift.print(RealmService.shared.fetch(object: User.self))
+  }
+  
+  public init() {  }
+  
+}
