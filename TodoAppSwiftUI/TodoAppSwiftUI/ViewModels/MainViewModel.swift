23707912407d82537ac1d4d96517e687ea2d44bc
NicholasPilotto <nicholaspilotto@gmail.com>
2023-06-20 21:37:56 +0200

    isSignIn stored property

--- TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/MainViewModel.swift
+++ TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/MainViewModel.swift
@@ -8,24 +8,9 @@
 import Foundation
 import RealmSwift
 
-class MainViewModel: ObservableObject {
-  @Published var appDataModel = AppDataModel()
-  
+class MainViewModel: ObservableObject {  
   public var isSignedIn: Bool {
-    let user = RealmService.shared.fetch(object: User.self)
-      .where {
-        $0._id.equals(appDataModel.userID)
-      }
-    
-    if !user.isEmpty {
-      return true
-    }
-    
-    return false
-  }
-  
-  public func print() {
-    Swift.print(RealmService.shared.fetch(object: User.self))
+      return !UserDefaultsService.shared.userID.isCompletelyEmpty
   }
   
   public init() {  }
