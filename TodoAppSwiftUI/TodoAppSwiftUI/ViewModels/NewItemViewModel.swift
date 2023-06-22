925c8da757b1c8e6714e650b01c322ec8dd60dc6
NicholasPilotto <nicholaspilotto@gmail.com>
2023-06-22 21:13:52 +0200

    Input data validation

--- TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/NewItemViewModel.swift
+++ TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/NewItemViewModel.swift
@@ -10,6 +10,19 @@ import Foundation
 class NewItemViewModel: ObservableObject {
   @Published var title = ""
   @Published var dueDate = Date()
+  @Published var showAlert = false;	
+  
+  var canSave: Bool {
+    guard !title.isCompletelyEmpty else {
+      return false
+    }
+    
+    guard dueDate >= Date().addingTimeInterval(-86400) else {
+      return false
+    }
+    
+    return true
+  }
   
   public init() {}
   
