523150cf547ab90f12c378417ec198a5c9340f7b
NicholasPilotto <nicholaspilotto@gmail.com>
2023-06-22 19:30:11 +0200

    New item view UI

--- TodoAppSwiftUI/TodoAppSwiftUI/Views/NewItemView.swift
+++ TodoAppSwiftUI/TodoAppSwiftUI/Views/NewItemView.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct NewItemView: View {
+  @StateObject var viewModel = NewItemViewModel()
+  @Binding var newItemPresented: Bool
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      VStack {
+        Text("New item")
+          .font(.system(size: 32))
+          .bold()
+          .padding(.top, 100)
+        
+        Form {
+          TextField("Title", text: $viewModel.title)
+            .textFieldStyle(DefaultTextFieldStyle())
+          
+          DatePicker("Due date", selection: $viewModel.dueDate)
+            .datePickerStyle(GraphicalDatePickerStyle())
+          
+          TLButton(title: "Save", background: .pink, action: {
+            viewModel.save()
+            newItemPresented = false
+          })
+          .padding()
+        }
+      }
     }
 }
 
-struct NewItemView_Previews: PreviewProvider {
-    static var previews: some View {
-        NewItemView()
-    }
-}
+//struct NewItemView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        NewItemView()
+//    }
+//}
