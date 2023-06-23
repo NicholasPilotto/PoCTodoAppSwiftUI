3045d1aedcda291aaddfc4a29ed4cb6a65c668ca
NicholasPilotto <nicholaspilotto@gmail.com>
2023-06-23 21:08:06 +0200

    ToDo item model

--- TodoAppSwiftUI/TodoAppSwiftUI/Models/ToDoListItem.swift
+++ TodoAppSwiftUI/TodoAppSwiftUI/Models/ToDoListItem.swift
@@ -8,6 +8,25 @@
 import Foundation
 import RealmSwift
 
-//class ToDoListItem: Object {
-//  
-//}
+class ToDoListItem: Object {
+  @Persisted var id: String
+  @Persisted var title: String
+  @Persisted var dueData: TimeInterval
+  @Persisted var createdDate: TimeInterval
+  @Persisted var isDone: Bool
+  
+  override init() {}
+  
+  public init(id: String, title: String, dueData: TimeInterval, createdDate: TimeInterval, isDone: Bool) {
+    super.init()
+    self.id = id
+    self.title = title
+    self.dueData = dueData
+    self.createdDate = createdDate
+    self.isDone = isDone
+  }
+  
+  public func setDone(_ state: Bool) {
+    self.isDone = state
+  }
+}
