3c470eafade37eefda02e529b2eeddf42f96aea4
NicholasPilotto <nicholaspilotto@gmail.com>
2023-06-25 17:21:34 +0200

    Get the list of todos for a specific user

--- TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/ToDoListViewModel.swift
+++ TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/ToDoListViewModel.swift
@@ -9,8 +9,24 @@ import Foundation
 
 class ToDoListViewModel: ObservableObject {
   @Published var showingNewItemView = false
+  @Published var items: [ToDoListItem] = []
   
-  public init() {}
+  public init() {
+    getAllTodos(userId: UserDefaultsService.shared.userID)
+  }
+  
+  public func getAllTodos(userId: String) {
+    do {
+      let tmp = try RealmService.shared.fetch(with: User.self)
+      tmp.where {
+        $0._id.equals(userId)
+      }.first?.todos.forEach({
+        self.items.append($0)
+      })
+    } catch {
+      print(error)
+    }
+  }
   
   public func delete(itemId: String) {
     print(itemId)
