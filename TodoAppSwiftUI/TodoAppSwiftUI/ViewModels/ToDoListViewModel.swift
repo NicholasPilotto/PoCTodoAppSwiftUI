560ccc4d5a68f89673f24ffd71766af42c59ca44
NicholasPilotto <nicholaspilotto@gmail.com>
2023-06-26 21:20:05 +0200

    Removed read items method

--- TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/ToDoListViewModel.swift
+++ TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/ToDoListViewModel.swift
@@ -9,24 +9,8 @@ import Foundation
 
 class ToDoListViewModel: ObservableObject {
   @Published var showingNewItemView = false
-  @Published var items: [ToDoListItem] = []
   
-  public init() {
-    getAllTodos(userId: UserDefaultsService.shared.userID)
-  }
-  
-  public func getAllTodos(userId: String) {
-    do {
-      let tmp = try RealmService.shared.fetch(with: User.self)
-      tmp.where {
-        $0._id.equals(userId)
-      }.first?.todos.forEach({
-        self.items.append($0)
-      })
-    } catch {
-      print(error)
-    }
-  }
+  public init() {}
   
   public func delete(itemId: String) {
     print(itemId)
