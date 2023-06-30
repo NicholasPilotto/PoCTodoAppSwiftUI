d8f0b1547e14362f785479d6733a29f1c80232a8
NicholasPilotto <nicholaspilotto@gmail.com>
2023-06-30 19:44:53 +0200

    observable list

--- TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/ToDoListViewModel.swift
+++ TodoAppSwiftUI/TodoAppSwiftUI/ViewModels/ToDoListViewModel.swift
@@ -8,11 +8,43 @@
 import Foundation
 
 class ToDoListViewModel: ObservableObject {
-  @Published var showingNewItemView = false
+  @Published var showingNewItemView = false {
+    didSet {
+      self.refreshItems(userId: UserDefaultsService.shared.userID)
+    }
+  }
+  @Published var items = [ToDoListItem]()
   
   public init() {}
   
-  public func delete(itemId: String) {
-    print(itemId)
+  public func refreshItems(userId: String) {
+    items.removeAll()
+
+    do {
+      let tmp = try RealmService.shared.fetch(with: User.self)
+      tmp.where {
+        $0._id.equals(userId)
+      }.first?.todos.forEach({
+        if (!$0.isInvalidated) {        
+          items.append($0)
+        }
+      })
+
+    } catch {
+    }
+
+  }
+  
+  public func delete(item: ToDoListItem) {
+    RealmService.shared.delete(object: item) { result in
+      switch result {
+        case .success():
+          break
+        case.failure(let error):
+          print(error)
+      }
+    }
+    
+//    self.refreshItems(userId: UserDefaultsService.shared.userID)
   }
 }
